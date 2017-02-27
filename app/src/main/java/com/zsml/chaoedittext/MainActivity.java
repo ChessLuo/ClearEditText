5d93bf1bbbffdbbf3d8c982c61f8e04874606149
zsml2016 <zsml2016@163.com>
2017-02-27 13:48:56 +0800

    Initial commit

---
+++ app/src/main/java/com/zsml/chaoedittext/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zsml.chaoedittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @描述 
+ * @author luoweichao
+ * @wechat 1329869063
+ * @date 2017/2/27 13:46
+ *
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
